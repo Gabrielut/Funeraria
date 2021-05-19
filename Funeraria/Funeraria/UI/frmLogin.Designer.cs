bc510b4be09e137f56df30bf0ce9e3bfcc5c9a46
kapizarro <78702866+kapizarro@users.noreply.github.com>
2021-05-19 00:58:27 -0600

    LoginChanges - frmLogin

--- Funeraria/Funeraria/UI/frmLogin.Designer.cs
+++ Funeraria/Funeraria/UI/frmLogin.Designer.cs
@@ -28,61 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pnlBarraTitulo = new System.Windows.Forms.Panel();
-            this.pnlContainer = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pnlBarraTitulo.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblBienvenido = new System.Windows.Forms.Label();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.picBoxLogo = new System.Windows.Forms.PictureBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblContrasena = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnRecuperarContrasena = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLogo)).BeginInit();
             this.SuspendLayout();
             // 
-            // pnlBarraTitulo
+            // panel1
             // 
-            this.pnlBarraTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
-            this.pnlBarraTitulo.Controls.Add(this.pictureBox1);
-            this.pnlBarraTitulo.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlBarraTitulo.Location = new System.Drawing.Point(0, 0);
-            this.pnlBarraTitulo.Name = "pnlBarraTitulo";
-            this.pnlBarraTitulo.Size = new System.Drawing.Size(1300, 35);
-            this.pnlBarraTitulo.TabIndex = 3;
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnRecuperarContrasena);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.lblBienvenido);
+            this.panel1.Controls.Add(this.txtContrasena);
+            this.panel1.Controls.Add(this.picBoxLogo);
+            this.panel1.Controls.Add(this.txtUsuario);
+            this.panel1.Controls.Add(this.btnLogin);
+            this.panel1.Controls.Add(this.lblUsuario);
+            this.panel1.Controls.Add(this.lblContrasena);
+            this.panel1.Location = new System.Drawing.Point(471, 106);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(351, 518);
+            this.panel1.TabIndex = 9;
             // 
-            // pnlContainer
+            // panel3
             // 
-            this.pnlContainer.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pnlContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlContainer.Location = new System.Drawing.Point(0, 35);
-            this.pnlContainer.Name = "pnlContainer";
-            this.pnlContainer.Size = new System.Drawing.Size(1300, 615);
-            this.pnlContainer.TabIndex = 5;
+            this.panel3.BackColor = System.Drawing.Color.Gray;
+            this.panel3.Location = new System.Drawing.Point(90, 250);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 2);
+            this.panel3.TabIndex = 8;
             // 
-            // pictureBox1
+            // lblBienvenido
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(1263, 7);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.lblBienvenido.AutoSize = true;
+            this.lblBienvenido.BackColor = System.Drawing.Color.Transparent;
+            this.lblBienvenido.Font = new System.Drawing.Font("Microsoft JhengHei UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBienvenido.ForeColor = System.Drawing.Color.Goldenrod;
+            this.lblBienvenido.Location = new System.Drawing.Point(70, 26);
+            this.lblBienvenido.Name = "lblBienvenido";
+            this.lblBienvenido.Size = new System.Drawing.Size(220, 47);
+            this.lblBienvenido.TabIndex = 1;
+            this.lblBienvenido.Text = "Bienvenido";
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtContrasena.Location = new System.Drawing.Point(93, 316);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(160, 13);
+            this.txtContrasena.TabIndex = 7;
+            // 
+            // picBoxLogo
+            // 
+            this.picBoxLogo.Image = ((System.Drawing.Image)(resources.GetObject("picBoxLogo.Image")));
+            this.picBoxLogo.Location = new System.Drawing.Point(135, 89);
+            this.picBoxLogo.Name = "picBoxLogo";
+            this.picBoxLogo.Size = new System.Drawing.Size(89, 71);
+            this.picBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxLogo.TabIndex = 0;
+            this.picBoxLogo.TabStop = false;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsuario.Location = new System.Drawing.Point(94, 223);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(180, 13);
+            this.txtUsuario.TabIndex = 6;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.Location = new System.Drawing.Point(90, 430);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(158, 32);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(89, 201);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(65, 19);
+            this.lblUsuario.TabIndex = 2;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblContrasena
+            // 
+            this.lblContrasena.AutoSize = true;
+            this.lblContrasena.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContrasena.Location = new System.Drawing.Point(90, 285);
+            this.lblContrasena.Name = "lblContrasena";
+            this.lblContrasena.Size = new System.Drawing.Size(93, 19);
+            this.lblContrasena.TabIndex = 3;
+            this.lblContrasena.Text = "Contraseña";
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1300, 33);
+            this.panel2.TabIndex = 10;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Gray;
+            this.panel4.Location = new System.Drawing.Point(90, 335);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 2);
+            this.panel4.TabIndex = 9;
+            // 
+            // btnRecuperarContrasena
+            // 
+            this.btnRecuperarContrasena.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.btnRecuperarContrasena.FlatAppearance.BorderSize = 0;
+            this.btnRecuperarContrasena.ForeColor = System.Drawing.Color.White;
+            this.btnRecuperarContrasena.Location = new System.Drawing.Point(197, 352);
+            this.btnRecuperarContrasena.Name = "btnRecuperarContrasena";
+            this.btnRecuperarContrasena.Size = new System.Drawing.Size(138, 23);
+            this.btnRecuperarContrasena.TabIndex = 10;
+            this.btnRecuperarContrasena.Text = "Recuperar Contraseña";
+            this.btnRecuperarContrasena.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1300, 650);
-            this.Controls.Add(this.pnlContainer);
-            this.Controls.Add(this.pnlBarraTitulo);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1300, 700);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.pnlBarraTitulo.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLogo)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel pnlBarraTitulo;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Panel pnlContainer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblBienvenido;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.PictureBox picBoxLogo;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblContrasena;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnRecuperarContrasena;
     }
 }
 
