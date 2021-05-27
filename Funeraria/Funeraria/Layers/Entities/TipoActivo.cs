1d446d6ebe1ddc91c727ffde69a1e0849c178946
Gabriel Torres <gtorress106@gmail.com>
2021-05-26 18:23:32 -0600

    Todos los entities creados

---
+++ Funeraria/Funeraria/Layers/Entities/TipoActivo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTN.Winform.Funeraria.Layers.Entities
+{
+    class TipoActivo
+    {
+        public int IdTipoActivo { get; set; }
+        public string Descripcion { get; set; }
+        public bool Estado { get; set; }
+    }
+}
